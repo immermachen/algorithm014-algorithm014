56503875ae7d29c787a40af086d83e5e2394c0c3
Joel <yang.zhang0049@hotmail.com>
2020-08-14 11:43:08 +0200

    update <42.trapping-rain-water>

--- Week_01/42.trapping-rain-water.cpp
+++ Week_01/42.trapping-rain-water.cpp
@@ -7,33 +7,33 @@
 // @lc code=start
 class Solution {
 public:
-    //Idea: https://leetcode.com/problems/trapping-rain-water/discuss/17391/Share-my-short-solution.
+    //Idea: https://leetcode.com/problems/trapping-rain-water/discuss/17364/7-lines-C-C%2B%2B
     int trap(vector<int>& height) {
-        int water=0;
-        int l=0, r=height.size()-1, lmax=0, rmax=0;
+        int water=0, r=height.size()-1, l=0, level=0;
         while(l<r) {
-            lmax = max(lmax, height[l]);
-            rmax = max(rmax, height[r]);
-            if(lmax < rmax) {
-                water += lmax-height[l++];
-            } else {
-                water += rmax-height[r--];                
-            }
+            int lower = height[height[l]<height[r] ? l++ : r--];
+            level = max(level, lower);
+            water += level - lower;
         }
         return water;
     }
-    
-    // //Idea: https://leetcode.com/problems/trapping-rain-water/discuss/17364/7-lines-C-C%2B%2B
+
+    // //Idea: https://leetcode.com/problems/trapping-rain-water/discuss/17391/Share-my-short-solution.
     // int trap(vector<int>& height) {
-    //     int water=0, r=height.size()-1, l=0, level=0;
+    //     int water=0;
+    //     int l=0, r=height.size()-1, lmax=0, rmax=0;
     //     while(l<r) {
-    //         int lower = height[height[l]<height[r] ? l++ : r--];
-    //         level = max(level, lower);
-    //         water += level - lower;
+    //         lmax = max(lmax, height[l]);
+    //         rmax = max(rmax, height[r]);
+    //         if(lmax < rmax) {
+    //             water += lmax-height[l++];
+    //         } else {
+    //             water += rmax-height[r--];                
+    //         }
     //     }
     //     return water;
     // }
-    
+
     // //Idea: https://leetcode.com/problems/trapping-rain-water/discuss/17357/Sharing-my-simple-c%2B%2B-code%3A-O(n)-time-O(1)-space
     // int trap(vector<int>& height) {
     //     int left=0; int right=height.size()-1;
